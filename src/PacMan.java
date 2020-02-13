548cfee457d4dce6807ce92761b8fa55e1ddb55e
Idris01 <Idrys01@gmail.com>
2020-02-13 10:43:41 +0100

    Revert "improved"
    This reverts commit 7f34a17883e5bfc436fee9e1c57d4b9c4c53c93f.

--- src/PacMan.java
+++ src/PacMan.java
@@ -11,20 +11,4 @@ public class PacMan {
         this.locX = locX;
         this.locY = locY;
     }
-
-    public void setLocX(int x) {
-        locX = x;
-    }
-
-    public int getLocX() {
-        return locX;
-    }
-
-    public void setLocY(int y) {
-        locY = y;
-    }
-
-    public int getLocY() {
-        return locY;
-    }
 }
--- src/PacmanGameGUI.java
+++ src/PacmanGameGUI.java
@@ -3,7 +3,6 @@ import java.awt.*;
 
 public class PacmanGameGUI extends JFrame {
     private static PacMan pMan;
-    private final static int pManDiameter = 20;
     private static GameMap map;
     private static AnimationPanel animate;
 
@@ -11,18 +10,12 @@ public class PacmanGameGUI extends JFrame {
         // gets the system Display size
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int w = (int) screenSize.getWidth() / 2;
-        int h = (int) screenSize.getHeight() / 2;
+        int h = (int) screenSize.getHeight() / 3;
 
-        setTitle("PAC MAN");
         setSize(new Dimension(w, h));
         map = new GameMap(w, h, 0);
-        Point loc = map.getLocation();
-        pMan = new PacMan(loc.x, loc.y);
-        animate = new AnimationPanel();
+        pMan = new PacMan()
         setContentPane(animate);
-        setResizable(false);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
 
 
     }
@@ -31,22 +24,7 @@ public class PacmanGameGUI extends JFrame {
 
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
-            Graphics2D g2 = (Graphics2D) g;
-
-            //set the rendering hint, this makes the drawing smoother
-            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                    RenderingHints.VALUE_ANTIALIAS_ON);
-
-            // draw the pacman
-            g.setColor(Color.BLUE);
-            g.fillOval(pMan.getLocX() - pManDiameter / 2, pMan.getLocY() - pManDiameter / 2, pManDiameter, pManDiameter);
-
-
         }
-    }
-
-    public static void main(String[] args) {
-        new PacmanGameGUI();
     }
 
 }
